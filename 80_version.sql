c56fb742a2c35cf77507bf84d4069d97d89eaf04
unleo <dselyanin@gmail.com>
2018-06-09 10:13:22 +0300

    issue #1, удаление файлов версионности

--- 80_version.sql
+++
@@ -1,5 +0,0 @@
-
-
--- ----------------------------------------------------------------------------
-DROP FUNCTION package_version();
-ALTER FUNCTION package_version_new() RENAME TO package_version;
